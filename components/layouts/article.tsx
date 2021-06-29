9e23739d03d84c8ba8db00ddd4ddc35366579fdc
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-06-29 19:21:31 -0400

    working on creating blog post template

--- components/layouts/article.tsx
+++ components/layouts/article.tsx
@@ -1,9 +1,21 @@
+import {MDXProvider} from '@mdx-js/react'
+
 const Article = ({children, meta}) => {
     return (
-        <div className="mx-20 text-gray-700">
-            <h1 className="text-gray-500">{meta.title}</h1>
-            {children}
-        </div>
+        <MDXProvider
+            components={{
+                h1: (props) => <h1 {...props} className="text-xl font-light" />,
+                p: (props) => <p {...props} className="my-5" />,
+                ol: (props) => <ol {...props} className="my-5" />,
+                li: (props) => <li {...props} className="my-2 list-disc"  />
+             }}>
+            <div className="mx-20 my-20 text-gray-700">
+                <h1 className="text-gray-500">{meta.title}</h1>
+                <p>Published {meta.published}</p>
+                {children}
+            </div>
+        </MDXProvider>
+        
     )
 }
 
