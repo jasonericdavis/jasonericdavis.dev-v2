c329dfca0619ae97552b502c28bafad6933d283c
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-06-29 20:40:52 -0400

    more work on layout

--- components/layouts/article.tsx
+++ components/layouts/article.tsx
@@ -5,15 +5,16 @@ const Article = ({children, meta}) => {
         <MDXProvider
             components={{
                 h1: (props) => <h1 {...props} className="text-xl font-light" />,
+                h3: (props) => <><h3 {...props} className="font-bold mt-5"/><hr/></>,
                 p: (props) => <p {...props} className="my-5" />,
                 ol: (props) => <ol {...props} className="my-5" />,
                 li: (props) => <li {...props} className="my-2 list-disc"  />
              }}>
-            <div className="mx-20 my-20 text-gray-700">
-                <h1 className="text-gray-500">{meta.title}</h1>
+            <article className="mx-20 my-20 text-gray-700">
+                <h1 className="mb-10">{meta.title}</h1>
                 <p>Published {meta.published}</p>
                 {children}
-            </div>
+            </article>
         </MDXProvider>
         
     )
