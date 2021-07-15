1327b35c7da48328a4a38ce16fe09dc95a11001a
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-07-14 22:09:23 -0400

    Added worth every penny post

--- components/layouts/article.tsx
+++ components/layouts/article.tsx
@@ -8,7 +8,8 @@ const Article = ({children, meta}) => {
                 h3: (props) => <><h3 {...props} className="font-bold mt-5"/><hr/></>,
                 p: (props) => <p {...props} className="my-5" />,
                 ol: (props) => <ol {...props} className="my-5" />,
-                li: (props) => <li {...props} className="my-2 list-disc"  />
+                li: (props) => <li {...props} className="my-2 list-disc"  />, 
+                blockquote: (props) => <blockquote {...props} style={{marginLeft: 30, fontSize: 'x-large'}}/>
              }}>
             <article className="mx-20 my-20 text-gray-700">
                 <h1 className="my-5">{meta.title}</h1>
