76a4aa3e6aea2730c328a9dbf66d2426d636e372
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-07-19 19:47:19 -0400

    Fix: #8 add favicons (#10)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
+import Head from 'next/head'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
     <div className="max-w-screen-xl mx-auto">
       <Component {...pageProps} />
     </div>
+    </>
   )
 }
 
